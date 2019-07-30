c28292b7701622c7ff6f7955867b19b97211bd5a
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-30 18:50:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,24 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize
+    @name = name 
+    @artist_name = artist_name
+  end
 
   def self.all
     @@all
   end
 
+  def self.create 
+    @@all << self 
+  end
+
   def save
     self.class.all << self
   end
+  
+  
 
 end
